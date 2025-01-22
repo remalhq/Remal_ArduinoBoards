384b4b0179def013126989f51df1b847d9f81c6f
0xKhalid <khalid@remal.io>
2025-01-23 00:12:20 +0400

    Added new esp32 release v1.3.0

--- arch/esp32/1.3.0/variants/shabakah_fe/pins_arduino.h
+++ arch/esp32/1.3.0/variants/shabakah_fe/pins_arduino.h
@@ -4,33 +4,8 @@
 #include <stdint.h>
 #include "soc/soc_caps.h"
 
-#define EXTERNAL_NUM_INTERRUPTS 22
-#define NUM_DIGITAL_PINS        22
-#define NUM_ANALOG_INPUTS       6
-
 /*
- * Changed for Shabakah (Founders Edition)
- */
-static const uint8_t RGB_LED_PIN = 6;
-static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+RGB_LED_PIN;
-#define BUILTIN_LED  LED_BUILTIN // backward compatibility
-#define LED_BUILTIN LED_BUILTIN
-#define RGB_BUILTIN LED_BUILTIN
-
-#define analogInputToDigitalPin(p)  (((p)<NUM_ANALOG_INPUTS)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
-#define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
-
-static const uint8_t TX = 21;
-static const uint8_t RX = 20;
-
-static const uint8_t SDA = 2;									//Changed for Shabakah (Founders Edition)
-static const uint8_t SCL = 8;									//Changed for Shabakah (Founders Edition)
-
-
-
-/*
- * Added for Shabakah (Founders Edition)
+ * Shabakah Founders Edition Specific (Anything added or changed): 
  */
 #define SHBK_BOARD
 #define SHBK_FE
@@ -38,11 +13,29 @@ static const uint8_t SCL = 8;									//Changed for Shabakah (Founders Edition)
 #define SHBK_BTN_BOOT			9
 #define SHBK_I2C_TEMP_ADDR		0x44
 
+static const uint8_t SDA = 2;
+static const uint8_t SCL = 8;
 
-static const uint8_t SS    = 7;
-static const uint8_t MOSI  = 6;
-static const uint8_t MISO  = 5;
-static const uint8_t SCK   = 4;
+#define PIN_RGB_LED             6       //Main LED on board
+
+/*
+ * Common with ESP32C3 (Unchanged values): 
+ */
+// BUILTIN_LED can be used in new Arduino API digitalWrite() like in Blink.ino
+static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
+#define BUILTIN_LED LED_BUILTIN  // backward compatibility
+#define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
+// RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
+#define RGB_BUILTIN    LED_BUILTIN
+#define RGB_BRIGHTNESS 50
+
+static const uint8_t TX = 21;
+static const uint8_t RX = 20;
+
+static const uint8_t SS = 7;
+static const uint8_t MOSI = 6;
+static const uint8_t MISO = 5;
+static const uint8_t SCK = 4;
 
 static const uint8_t A0 = 0;
 static const uint8_t A1 = 1;
